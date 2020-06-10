85689df3922bfde19dde13d9cb5d1c87abf3398b
MxFxM <mxfxmmueller@gmail.com>
2020-06-10 22:26:10 +0200

    changed ip and topic

--- server.py
+++ server.py
@@ -1,25 +1,29 @@
 import paho.mqtt.client as mqtt
 
+BROKER_IP = "192.168.178.61" # this is my local mqtt broker
+BROKER_PORT = 1883         # standard mqtt broker port
+BROKER_TOPIC = "Games/Pong"
+
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
-    print("Connected with result code "+str(rc))
+    if str(rc) == "0":
+        print("Connected with broker")
+    else:
+        print("Connected with result code "+str(rc))
 
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
-    client.subscribe("$SYS/#")
+    client.subscribe(BROKER_TOPIC)
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
-    print(msg.topic+" "+str(msg.payload))
+    print(str(msg.payload))
 
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
 
-client.connect("mqtt.eclipse.org", 1883, 60)
+client.connect(BROKER_IP, BROKER_PORT, 60)
 
-# Blocking call that processes network traffic, dispatches callbacks and
-# handles reconnecting.
-# Other loop*() functions are available that give a threaded interface and a
-# manual interface.
-client.loop_forever()
+while True:
+    client.loop()
