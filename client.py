a5a03a8c806b335ff77432df91e268db37bea204
MxFxM <mxfxmmueller@gmail.com>
2020-06-10 22:31:31 +0200

    added connection message for client

--- client.py
+++ client.py
@@ -1,8 +1,11 @@
 import paho.mqtt.client as mqtt
+import paho.mqtt.publish as publish
+import time
 
 BROKER_IP = "192.168.178.61" # this is my local mqtt broker
-BROKER_PORT = 1883         # standard mqtt broker port
+BROKER_PORT = 1883           # standard mqtt broker port
 BROKER_TOPIC = "Games/Pong"
+CLIENT_ID = time.time()      # use time as id
 
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
@@ -24,6 +27,8 @@ client.on_connect = on_connect
 client.on_message = on_message
 
 client.connect(BROKER_IP, BROKER_PORT, 60)
+
+publish.single(BROKER_TOPIC, payload="client online", hostname=BROKER_IP, port=BROKER_PORT)
 
 while True:
     client.loop()
